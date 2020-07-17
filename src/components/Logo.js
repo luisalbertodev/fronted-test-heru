e220eb1764fd207b862d4d4eaa5ace5dde7a60e2
Luis Alberto Perez Coello <luisalbertodev@gmail.com>
2020-07-17 03:22:56 -0500

    se agregaron los estilos JSX a los componentes correctamente

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Row, Col } from 'react-flexbox-grid';
 import LOGO from '../images/logo.svg';
 
+const style = {
+	wrapperImg: {
+		width: '100%',
+		height: 'auto',
+	},
+};
+
 export const Logo = () => {
 	return (
 		<Row center="xs">
 			<Col xs>
-				<img src={LOGO} alt="logotipo" className="img-fluid" />
+				<img src={LOGO} alt="logotipo" style={style.wrapperImg} />
 			</Col>
 		</Row>
 	);
