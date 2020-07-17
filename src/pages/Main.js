6dce2efc3068f217ad24a6dff0344813ead3fb7f
Luis Alberto Perez Coello <luisalbertodev@gmail.com>
2020-07-17 03:14:58 -0500

    Página principal conectada al estado global de redux

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Logo } from '../components/Logo';
+import { FilterGiphy } from '../components/FilterGiphy';
+import { CardItemGif } from '../components/CardItemGif';
+import { EmptyData } from '../components/EmptyData';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import { connect } from 'react-redux';
+import { fetchTopGiphy, fetchFindGiphy } from '../redux/actions/fetchTopGiphy';
 
-import { InputGroup, FormControl } from 'react-bootstrap';
-// import PropTypes from 'prop-types';
+class Main extends Component {
+	state = { text_filter: '' };
 
-export const Main = () => {
-	return (
-		<Grid>
-			<Row center="xs">
-				<Col xs={6} md={2}>
-					<Logo />
-				</Col>
-			</Row>
-			<Row>
-				<Col xs={12}>
-					<InputGroup size="sm" className="mb-3">
-						<InputGroup.Prepend>
-							<InputGroup.Text id="inputGroup-sizing-sm">Small</InputGroup.Text>
-						</InputGroup.Prepend>
-						<FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
-					</InputGroup>
-				</Col>
-			</Row>
-			<Row>
-				<Col xs={12}>
-					<p>asdasdsadasdas</p>
-				</Col>
-			</Row>
-		</Grid>
-	);
+	handleOnChangeInput = (event) => {
+		const { name, value } = event.target;
+		this.setState({ [name]: value });
+	};
+
+	handleOnSubmit = (e) => {
+		e.preventDefault();
+		this.props.fetchFindGiphy({ payload: this.state.text_filter });
+	};
+
+	componentDidMount() {
+		this.props.fetchTopGiphy();
+	}
+
+	componentDidUpdate(prevState, prevProps) {
+		if (prevProps.results !== this.props.results) {
+			if (Array.isArray(this.props.results) && this.props.results.length === 0) {
+				this.props.fetchTopGiphy();
+			}
+		}
+	}
+
+	render() {
+		const { results } = this.props;
+		return (
+			<Grid>
+				<Row center="xs">
+					<Col xs={6} md={2}>
+						<Logo />
+					</Col>
+				</Row>
+				<FilterGiphy onSubmit={this.handleOnSubmit} onInput={this.handleOnChangeInput} />
+				{results.data !== undefined ? <CardItemGif data={results.data} /> : <EmptyData />}
+			</Grid>
+		);
+	}
+}
+
+const mapStateToProps = (state) => ({
+	results: state.getData,
+});
+
+const mapDispatchToProps = {
+	fetchTopGiphy,
+	fetchFindGiphy,
 };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
